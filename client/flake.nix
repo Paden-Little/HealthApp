ecc371cd0c848cfbafe2f9aaeb78e1e5a8bcd3da
RatRyan <ryantratajczak@gmail.com>
2024-05-03 17:54:18 -0600

    refresh client

--- client/flake.nix
+++ client/flake.nix
@@ -9,10 +9,10 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { pkgs, ... }: {
         devShells = {
           default = pkgs.mkShell {
-            packages = with pkgs; [bun];
+            packages = with pkgs; [nodejs];
           };
         }; 
       };
