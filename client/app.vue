4182f15c30022453f5640918767b559905becf56
RatRyan <ryantratajczak@gmail.com>
2024-05-03 18:46:22 -0600

    add small demo for testing http functionality

--- client/app.vue
+++ client/app.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+const responses = reactive<string[]>([]);
 
+async function getServerData() {
+  const resp = await $fetch<string>('/api/record');
+  responses.push(resp)
+}
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <button @click="getServerData()">GET http://localhost:3000</button>
+  <li v-for="response in responses">
+    {{ response }}
+  </li>
 </template>
