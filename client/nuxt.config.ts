4eab6b9f1fefbb726719a4650962616bac125483
RatRyan <ryantratajczak@gmail.com>
2024-05-08 05:49:42 +0000

    remove tailwind module

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,10 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   devServer: {
     port: 3001,
   },
-
-  modules: ["@nuxtjs/tailwindcss"],
 });
