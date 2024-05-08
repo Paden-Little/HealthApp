cf2f04d2e89546cb81cdbe57fd35ff749f919613
RatRyan <ryantratajczak@gmail.com>
2024-05-08 06:19:31 +0000

    fix tailwind configuration

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: true,
+    // and more...
+  },
   devServer: {
     port: 3001,
   },
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>{{ $route.path }}</h1>
+  <h1 class="">{{ $route.path }}</h1>
 </template>
