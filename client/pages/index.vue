3425afc18a793f430f09875bea8583f46213ea1a
RatRyan <ryantratajczak@gmail.com>
2024-05-09 11:47:52 -0600

    add .prettierrc (for tailwind sorting)

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,24 +1,24 @@
 <template>
   <section class="bg-[url('/img/hero-bg.png')]">
-    <div class="grid max-w-screen-xl mx-auto gap-0 pt-16 grid-cols-12">
+    <div class="mx-auto grid max-w-screen-xl grid-cols-12 gap-0 pt-16">
       <div class="mr-auto place-self-center lg:col-span-7">
-        <h1 class="max-w-2xl mb-4 text-6xl font-extrabold text-white">
+        <h1 class="mb-4 max-w-2xl text-6xl font-extrabold text-white">
           Your Health,
         </h1>
         <h1
-          class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white"
+          class="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-6xl"
         >
           Our Priority
         </h1>
         <p
-          class="max-w-2xl mb-6 font-normal lg:mb-8 md:text-lg lg:text-xl text-white"
+          class="mb-6 max-w-2xl font-normal text-white md:text-lg lg:mb-8 lg:text-xl"
         >
           Someone put something else here
         </p>
         <button class="cta-button">Book an Appointment</button>
         <button class="glass-button">Find a Doctor</button>
       </div>
-      <div class="lg:mt-0 lg:col-span-5 lg:flex">
+      <div class="lg:col-span-5 lg:mt-0 lg:flex">
         <img src="/img/tricycle.png" />
       </div>
     </div>
