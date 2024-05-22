697c7a91563b8c6dfbe46b7d984b4db6cfc5b402
Robert Shirts <75270197+robertjshirts@users.noreply.github.com>
2024-05-21 21:51:34 -0600

    Update init.sql and migration.sql scripts

--- schemas/migration.sql
+++ schemas/migration.sql
@@ -1,11 +1,33 @@
+-- Make emails unique
 ALTER TABLE `provider`.`provider`
-ADD CONSTRAINT `unique_email` UNIQUE (`email`);
+ADD UNIQUE (`email`);
 
+ALTER TABLE `patient`.`patient`
+ADD UNIQUE (`email`);
+
+-- Add password field
 ALTER TABLE `provider`.`provider`
 ADD COLUMN `password` VARCHAR(255) NOT NULL DEFAULT '';
 
 ALTER TABLE `patient`.`patient`
-ADD CONSTRAINT `unique_email` UNIQUE (`email`);
+ADD COLUMN `password` VARCHAR(255) NOT NULL DEFAULT '';
+
+-- Make fields not null
+ALTER TABLE `patient`.`patient`
+MODIFY COLUMN `email` VARCHAR(255) NOT NULL;
 
 ALTER TABLE `patient`.`patient`
-ADD COLUMN `password` VARCHAR(255) NOT NULL DEFAULT '';
+MODIFY COLUMN `language` INT NOT NULL;
+
+ALTER TABLE `provider`.`provider`
+MODIFY name VARCHAR(255) NOT NULL;
+
+ALTER TABLE `provider`.`provider`
+MODIFY suffix VARCHAR(255) NOT NULL;
+
+ALTER TABLE `provider`.`provider`
+MODIFY bio TEXT NOT NULL;
+
+ALTER TABLE `provider`.`provider`
+MODIFY email VARCHAR(255) NOT NULL;
+
