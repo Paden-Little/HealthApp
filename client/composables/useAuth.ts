e3f9ee1a17de3be0852905c4ca31653a4036005b
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-05-28 08:31:04 -0600

    merge composables

--- client/composables/useAuth.ts
+++ client/composables/useAuth.ts
@@ -1,21 +1,32 @@
 import axios from "axios";
+const bcrypt = require('bcryptjs');
+const fixedSalt = "$2a$10$1234567890123456789012";
+
+const hashPassword = async (password: string) => {
+  return await bcrypt.hash(password, fixedSalt);
+}
+
 
 export function useAuth() {
   const registerPatient = async (patient: any) => {
+    patient.password = await hashPassword(patient.password);
     return await axios.post("127.0.0.1/patient", patient);
   }
 
   const registerProvider = async (provider: any) => {
+    provider.password = await hashPassword(provider.password);
     return await axios.post("127.0.0.1/provider", provider);
   }
 
   const loginPatient = async (patient: any) => {
+    patient.password = await hashPassword(patient.password);
     return await axios.post("127.0.0.1/patient/login", patient);
   }
 
   const loginProvider = async (provider: any) => {
+    provider.password = await hashPassword(provider.password);
     return await axios.post("127.0.0.1/provider/login", provider);
   }
 
-  return {registerPatient, registerProvider, loginPatient, loginProvider};
+  return { registerPatient, registerProvider, loginPatient, loginProvider };
 }
