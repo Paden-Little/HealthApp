743ba735cfca74b102bc88c5efdacc9e88abb72f
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-05-28 16:57:14 -0600

    fixed login link

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -20,7 +20,9 @@ onMounted(() => {
           >
         </NuxtLink>
         <div class="flex items-center lg:order-2">
-          <a href="#" class="secondary-btn">Log in</a>
+          <!-- <a href="#" class="secondary-btn">Log in</a> -->
+          <NuxtLink to="/my-health/authentication/login" class="secondary-btn"
+            >Log in</NuxtLink>
           <NuxtLink to="/find-a-provider" class="cta-btn"
             >Book an Appointment</NuxtLink
           >
