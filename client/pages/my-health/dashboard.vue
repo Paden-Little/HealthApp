299250dcaee75be9ad216c90aa56f9666233f203
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-05-28 15:49:25 -0600

    update dashboard

--- client/pages/my-health/dashboard.vue
+++ client/pages/my-health/dashboard.vue
@@ -1,46 +1,40 @@
 <script setup lang="ts">
 const patient = ref<Patient>();
 
-onBeforeMount(async () => {
-  try {
-    patient.value = await useAuth().getPatientData();
-  } catch (error) {
-    console.error(error);
-  }
+onBeforeMount(() => {
+  useAuth()
+    .getPatientData()
+    .then(data => {
+      patient.value = data.value as Patient;
+    });
 });
 </script>
 <template>
-  <!-- TEMP TEST -->
-  {{ patient }}
   <div v-if="patient">
     <h2 class="">Patient Information</h2>
-    <p>Fullname: {{ patient.firstname + " " + patient.lastname }}</p>
-    <!-- <p>Fullname: {{ patient.value.firstname + ' ' + patient.value.lastname}}</p>
-    <p>Email: {{ patient.value.email }}</p>
-    <p>Phone: {{ patient.value.phone }}</p>
-    <p>Birthdate: {{ patient.value.birth }}</p>
+    <p>Fullname: {{ patient.firstname + ' ' + patient.lastname }}</p>
+    <p>Email: {{ patient.email }}</p>
+    <p>Birthdate: {{ patient.birth }}</p>
     <p>Allergies</p>
     <ul>
-      <li v-if="patient.value.allergies.count == 0">No allergies</li>
-      <li v-else v-for="allergy in patient.value.allergies">
+      <li v-if="patient.allergies.length === 0">No allergies</li>
+      <li v-else v-for="allergy in patient.allergies">
         <p>{{ allergy.name }}</p>
-        <br>
+        <br />
         <p>{{ allergy.description }}</p>
       </li>
     </ul>
-    <p>Current Perscriptions</p>
+    <p>Perscriptions</p>
     <ul>
-      <li v-if="patient.value.perscriptions.count == 0">No perscriptions</li>
-      <li v-else v-for="persription in patient.value.perscriptions">
-        <p>{{ persription.name }}</p>
-        <p>Dosage: {{ persription.dosage }} {{ persription.freqency }}</p>
-        <p>{{ persription.start }} - {{ persription.end }}</p>
+      <li v-if="patient.prescriptions.length === 0">No perscriptions</li>
+      <li v-else v-for="perscription in patient.prescriptions">
+        <p>{{ perscription.name }}</p>
+        <p>Dosage: {{ perscription.dosage }} {{ perscription.frequency }}</p>
+        <p>{{ perscription.start }} - {{ perscription.end }}</p>
       </li>
-    </ul> -->
+    </ul>
   </div>
   <div v-else>
-    <p>
-      something went wrong
-    </p>
+    <p>something went wrong - No user...</p>
   </div>
 </template>
