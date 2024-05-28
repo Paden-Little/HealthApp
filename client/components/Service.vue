7694cab222c921ea6903bad66511b7a5ebaec2b6
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-05-28 11:32:39 -0600

    init service component

---
+++ client/components/Service.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+  img: String,
+  description: String,
+});
+</script>
+<template>
+  <main class="border">
+    <img :src="img" />
+    <h1
+      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl"
+    >
+      {{ name }}
+    </h1>
+    <p>{{ description }}</p>
+  </main>
+</template>
